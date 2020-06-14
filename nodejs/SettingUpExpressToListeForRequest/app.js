2b0a8a459e17c7dcdb54192513e53f074c5bfde9
AnaMarquez <anamarquez.edu@hotmail.com>
2020-06-13 20:53:45 -0700

    ROUTING DEFINING URL ROUTES AND SENDING A RESPONSE

--- nodejs/SettingUpExpressToListeForRequest/app.js
+++ nodejs/SettingUpExpressToListeForRequest/app.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 
 app.set('port',3000);
+                      // req:Requestobject res:Response object
+/*app.get('/', function(req, res){
+    console.log("Get the homepage");
+    res.status(404)
+    .send('Express yourself');
+});*/
+
+app.get('/', function(req, res){
+    console.log("Get the homepage");
+    res.status(200)
+    .sendfile(path.join(__dirname,'public','index.html'));
+});
+
+app.get('/json', function(req, res){
+    console.log("Get the json");
+  res.status(200)
+    .json("{jsonData:true}");
+});
+
+app.get('/file', function(req, res){
+    console.log("Get the file");
+  res.status(200)
+    .sendFile(path.join(__dirname,'app.js'));
+});
 
 var server = app.listen(app.get('port'), function(){ //IS ASYNCHRONOUS
     var port = server.address().port;
     console.log('Magic happens on port ' + port);
 });
 
-
-/**
- * how to instantiate express app , 
- * set a port number using  variables, 
- * how to read a number using variables , 
- * listeng for request on a particular port '
- * app. liste method is asynchronous
- * 
-*/
