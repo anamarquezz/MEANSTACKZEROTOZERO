ef1c552550614a44520cdad5078bb8f12389814f
AnaMarquez <anamarquez.edu@hotmail.com>
2020-06-13 19:05:18 -0700

    bring expresstoarea, how to instantiate express app , set a port number using app variables, how to read a number, listeng for request on a particular port

--- nodejs/SettingUpExpressToListeForRequest/app.js
+++ nodejs/SettingUpExpressToListeForRequest/app.js
@@ -8,12 +8,13 @@ var server = app.listen(app.get('port'), function(){ //IS ASYNCHRONOUS
     var port = server.address().port;
     console.log('Magic happens on port ' + port);
 });
-console.log("Me first");
+
 
 /**
  * how to instantiate express app , 
  * set a port number using  variables, 
  * how to read a number using variables , 
  * listeng for request on a particular port '
+ * app. liste method is asynchronous
  * 
 */
