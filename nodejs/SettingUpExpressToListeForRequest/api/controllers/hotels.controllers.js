74a90dc098bc34cd526e32ddc01d31133e82374e
AnaMarquez <anamarquez.edu@hotmail.com>
2020-06-14 17:10:54 -0700

    defining dynamic routes with URL parameters

--- nodejs/SettingUpExpressToListeForRequest/api/controllers/hotels.controllers.js
+++ nodejs/SettingUpExpressToListeForRequest/api/controllers/hotels.controllers.js
@@ -4,4 +4,12 @@ module.exports.hotelsGetAll = function(req, res){
     console.log("Get the json");
     res.status(200)
       .json( hotelData );    
+};
+
+module.exports.hotelsGetOne = function(req, res){
+    var hotelId = req.params.hotelId;
+    var thisHotel = hotelData[hotelId];
+    console.log("Get hotelId", hotelId);
+    res.status(200)
+      .json( thisHotel );    
 }
