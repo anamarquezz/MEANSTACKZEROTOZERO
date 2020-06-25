acf99f9a604ad156ab3ebc418a51a2d8fdd2a4c0
AnaMarquez <anamarquez.edu@hotmail.com>
2020-06-24 22:39:14 -0700

    MONGODB Qurying the database from Node.js

--- nodejs/SettingUpExpressToListeForRequest/api/controllers/hotels.controllers.js
+++ nodejs/SettingUpExpressToListeForRequest/api/controllers/hotels.controllers.js
@@ -1,4 +1,5 @@
 var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
 module.exports.hotelsGetAll = function (req, res) {
@@ -11,46 +12,43 @@ module.exports.hotelsGetAll = function (req, res) {
     var offset = 0;
     var count = 5;
 
-    if(req.query && req.query.offset){
+    if (req.query && req.query.offset) {
         offset = parseInt(req.query.offset, 10);
     }
 
-    if(req.query && req.query.count){
+    if (req.query && req.query.count) {
         count = parseInt(req.query.count, 10);
     }
-        
+
     collection
-    .find()
-    .skip
-    .toArray(function (err, docs) {
-        console.log('Found hotels', docs);
-        res.status(200)
-            .json(docs);
-    });
+        .find()
+        .skip(offset)
+        .limit(count)
+        .toArray(function (err, docs) {
+            console.log('Found hotels', docs);
+            res
+                .status(200)
+                .json(docs);
+        });
 
-
-  
-   // console.log("db", db);
-
-
-  //  console.log("Get the json");
-  //  console.log(req.query);
-
-  
- /*   
-    var returnData = hotelData.slice(offset, offset + count);
-
-
-    res.status(200)
-      .json( returnData );    */
 };
-
 module.exports.hotelsGetOne = function (req, res) {
+
+    var db = dbconn.get();
+    var collection = db.collection('hotel');
+
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId];
     console.log("Get hotelId", hotelId);
-    res.status(200)
-        .json(thisHotel);
+
+    collection
+        .findOne({
+            _id:ObjectId(hotelId)
+        }, function (err, doc) {
+            res
+            .status(200)
+            .json(doc);
+        });
+
 };
 
 module.exports.hotelsAddOne = function (req, res) {
