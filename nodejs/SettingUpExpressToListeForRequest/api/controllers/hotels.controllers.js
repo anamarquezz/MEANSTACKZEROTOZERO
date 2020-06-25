f02ad3cecbfa899328bb33f18dc37658a9371dd6
AnaMarquez <anamarquez.edu@hotmail.com>
2020-06-24 23:45:01 -0700

    MongoDB - Creating documents from Node.js

--- nodejs/SettingUpExpressToListeForRequest/api/controllers/hotels.controllers.js
+++ nodejs/SettingUpExpressToListeForRequest/api/controllers/hotels.controllers.js
@@ -52,7 +52,30 @@ module.exports.hotelsGetOne = function (req, res) {
 };
 
 module.exports.hotelsAddOne = function (req, res) {
-    console.log('POST new hotel');
+    
+    var db = dbconn.get();
+    var collection = db.collection('hotel');
+    var newHotel;
+
+    console.log('POST new hotel');    
+    if(req.body && req.body.name && req.body.stars){
+        newHotel = req.body;
+        newHotel.stars = parseInt(req.body.stars,10);      
+        collection.insertOne(newHotel, function(err, response){
+            console.log(response);
+            console.log(response.ops);
+            res
+            .status(201)
+            .json(response.ops);
+        });   
+    }else{
+        console.log("Data missing body");
+        res
+        .status(400)
+        .json({ message :"Required data misssing from body"});
+        return
+    }
+    
     console.log(req.body);
     res
         .status(200)
